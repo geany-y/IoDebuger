f92f584385fa0e52e9236fa57ab0b5d706bddf95
geany-y <1y.xxxsmokyxxx.y1@gmail.com>
2016-01-09 01:07:23 +0900

    build finish / other bug modified

--- php/myfunc/debug.php
+++ php/myfunc/debug.php
@@ -1,5 +1,6 @@
 <?php
 function ezd($dump){
+	/*
 	$data = http_build_query($dump, "", "&");
 
 	//header
@@ -24,4 +25,23 @@ function ezd($dump){
 		echo 'サーバーから応答がありません';
 	}
 	exit();
+	*/
+	$datas = array();
+		$datas['phpvar'] = serialize($dump);
+		//ノードに予約情報をプッシュ
+		$url = 'http://localhost:3000/phpvar';
+		$header = array("Content-Type: application/json; charset=utf-8");
+		$options = array('http' => array(
+			'method' => 'POST',
+			'header' => implode("\r\n", $header),
+			//'content' => http_build_query($data)
+			'content' => json_encode($datas)
+		));
+	try{
+        $contents = file_get_contents($url, false, stream_context_create($options));
+        return true;
+	}catch(\Exception $e){
+		echo 'サーバーから応答がありません';
+	}
+    exit();
 }
